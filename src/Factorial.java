dae6452c1155ea82ed44bd02533c498b672999c2
Ivan Maltsev <ivan.maltsev78@gmail.com>
2018-06-08 10:57:30 +0500

    метод, вычисляющий факториал заданного натурального числа.

---
+++ src/Factorial.java
@@ -0,0 +1,22 @@
+import java.math.BigInteger;
+
+/**
+ * Реализуйте метод, вычисляющий факториал заданного натурального числа.
+ */
+public class Factorial {
+    public static void main(String[] args) {
+        BigInteger bigI = factorial(0);
+        System.out.println(bigI);
+    }
+
+    private static BigInteger factorial(int value) {
+        int i = 1;
+        BigInteger res = BigInteger.valueOf(1);
+        while (i <= value) {
+            res = res.multiply(BigInteger.valueOf(i));
+            i++;
+        }
+        return res;
+        return res;
+    }
+}
